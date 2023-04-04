e71734aeb4ab267ce8eb4f4173dccfbcb1c6b343
Judalm Park (dami) <wnekfa1004@naver.com>
2023-04-04 20:22:58 +0900

    @Component 스캔

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,7 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 
-@RequestMapping
+
+@RestController
 public class HelloController {
 
     private final HelloService helloService;
@@ -18,7 +21,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
