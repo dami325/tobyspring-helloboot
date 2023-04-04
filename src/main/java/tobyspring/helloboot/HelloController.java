2801aec9123dba3a9735387b0f01224284ce6a28
Judalm Park (dami) <wnekfa1004@naver.com>
2023-04-04 09:58:24 +0900

    Dependency Injection

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,16 +1,17 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Objects;
 
 
 public class HelloController {
 
-    public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
+    private final HelloService helloService;
 
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
